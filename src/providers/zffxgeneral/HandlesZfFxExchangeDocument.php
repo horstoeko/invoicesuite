bb83deaf14b751d018232dca651ca448122c209c
HorstOeko <horstoeko@erling.com.de>
2025-04-25 04:53:38 +0200

    [ENH] First for ZF/FX Minimum

---
+++ src/providers/zffxgeneral/HandlesZfFxExchangeDocument.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace horstoeko\invoicesuite\providers\zffxgeneral;
+
+use horstoeko\invoicesuite\concerns\HandlesRootObject;
+use horstoeko\invoicesuite\models\zffx\ram\DocumentContextParameterType;
+use horstoeko\invoicesuite\models\zffx\ram\ExchangedDocumentContextType;
+use horstoeko\invoicesuite\models\zffx\ram\ExchangedDocumentType;
+use horstoeko\invoicesuite\models\zffx\rsm\CrossIndustryInvoiceType;
+
+/**
+ * Trait for general Zugferd/Factur-X document handling
+ *
+ * @category InvoiceSuite
+ * @package  InvoiceSuite
+ * @author   horstoeko <horstoeko@erling.com.de>
+ * @license  https://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/horstoeko/invoicesuite
+ */
+trait HandlesZfFxExchangeDocument
+{
+    use HandlesRootObject;
+
+    /**
+     * Init Context parameter
+     *
+     * @param string $newContextParameter
+     * @return static
+     */
+    public function setContextParameter(string $newContextParameter): self
+    {
+        /**
+         * @var CrossIndustryInvoiceType $crossIndustryInvoice
+         */
+        $crossIndustryInvoice = $this->getRootObject();
+
+        $exchangedDocumentContextType = new ExchangedDocumentContextType();
+        $exchangedDocumentType = new ExchangedDocumentType();
+
+        $crossIndustryInvoice->setExchangedDocumentContext($exchangedDocumentContextType);
+        $crossIndustryInvoice->setExchangedDocument($exchangedDocumentType);
+
+        $documentContextParameterType = new DocumentContextParameterType();
+        $documentContextParameterType->getIDWithCreate()->setValue($newContextParameter);
+
+        $crossIndustryInvoice->getExchangedDocumentContext()->setGuidelineSpecifiedDocumentContextParameter($documentContextParameterType);
+
+        return $this;
+    }
+}
--- src/providers/zffxminimum/InvoiceSuiteZugferdFacturXMinimumProviderBuilder.php
+++ src/providers/zffxminimum/InvoiceSuiteZugferdFacturXMinimumProviderBuilder.php
@@ -2,8 +2,20 @@
 
 namespace horstoeko\invoicesuite\providers\zffxminimum;
 
+use horstoeko\invoicesuite\providers\zffxgeneral\HandlesZfFxExchangeDocument;
 use horstoeko\invoicesuite\abstracts\InvoiceSuiteAbstractFormatProviderBuilder;
 
 class InvoiceSuiteZugferdFacturXMinimumProviderBuilder extends InvoiceSuiteAbstractFormatProviderBuilder
 {
+    use HandlesZfFxExchangeDocument;
+
+    /**
+     * @inheritDoc
+     */
+    public function initRootObject(): InvoiceSuiteAbstractFormatProviderBuilder
+    {
+        $this->setContextParameter('urn:factur-x.eu:1p0:minimum');
+
+        return $this;
+    }
 }
