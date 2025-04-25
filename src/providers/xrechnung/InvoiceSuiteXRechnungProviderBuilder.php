a9c6e82b7df3f5d718fc9423efbfd4f6f90bce2f
HorstOeko <horstoeko@erling.com.de>
2025-04-25 05:45:31 +0200

    [ENH] Added XRechnung support

---
+++ src/providers/xrechnung/InvoiceSuiteXRechnungProviderBuilder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace horstoeko\invoicesuite\providers\xrechnung;
+
+use horstoeko\invoicesuite\abstracts\InvoiceSuiteAbstractFormatProviderBuilder;
+use horstoeko\invoicesuite\providers\zffx\HandlesZfFxExchangeDocument;
+use horstoeko\invoicesuite\providers\zffx\InvoiceSuiteZugferdFacturXProviderBuilder;
+
+class InvoiceSuiteXRechnungProviderBuilder extends InvoiceSuiteZugferdFacturXProviderBuilder
+{
+    use HandlesZfFxExchangeDocument;
+
+    /**
+     * @inheritDoc
+     */
+    public function initRootObject(): InvoiceSuiteAbstractFormatProviderBuilder
+    {
+        $this->setContextParameter(
+            'urn:cen.eu:en16931:2017#compliant#urn:xeinkauf.de:kosit:xrechnung_3.0',
+            'urn:fdc:peppol.eu:2017:poacc:billing:01:1.0'
+        );
+
+        return $this;
+    }
+}
---
+++ src/providers/xrechnung/InvoiceSuiteXRechnungProviderReader.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace horstoeko\invoicesuite\providers\xrechnung;
+
+use horstoeko\invoicesuite\providers\zffx\InvoiceSuiteZugferdFacturXProviderReader;
+
+class InvoiceSuiteXRechnungProviderReader extends InvoiceSuiteZugferdFacturXProviderReader
+{
+}
