8d260e298d0289448767cc4ad3e366959f91a218
HorstOeko <horstoeko@erling.com.de>
2025-12-17 12:42:44 +0100

    [ENH] Added ZugferdSpecificationVersions

---
+++ src/compat/zugferd/ZugferdSpecificationVersions.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is a part of horstoeko/invoicesuite
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace horstoeko\zugferd;
+
+/**
+ * Class representing the information source of the used specification versions
+ *
+ * @category InvoiceSuite
+ * @author   horstoeko <horstoeko@erling.com.de>
+ * @license  https://opensource.org/licenses/MIT MIT
+ * @see      https://github.com/horstoeko/invoicesuite
+ */
+class ZugferdSpecificationVersions
+{
+    /**
+     * The latest ZUGFeRD version used
+     */
+    public const VERSION_ZUGFERD = "2.3.2";
+
+    /**
+     * The latest Factur-X version used
+     */
+    public const VERSION_FACTURX = "1.07.2";
+
+    /**
+     * The latest XRechnung version used
+     */
+    public const VERSION_XRECHNUNG = "3.0.2";
+}
